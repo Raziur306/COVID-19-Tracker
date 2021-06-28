321e0f51b56f6612f3e680a75f0172272393f979
Md. Raziur Rahaman Ronju <75040026+Raziur306@users.noreply.github.com>
2021-06-28 23:15:14 +0600

    Close Icon Added

--- app/src/main/java/com/ronju/covid_19tracker/Activitys/MainActivity.java
+++ app/src/main/java/com/ronju/covid_19tracker/Activitys/MainActivity.java
@@ -1,13 +1,22 @@
 package com.ronju.covid_19tracker.Activitys;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.ronju.covid_19tracker.Activitys.Fragment.AboutActivity;
 import com.ronju.covid_19tracker.Activitys.Fragment.BDdataActivity;
 import com.ronju.covid_19tracker.Activitys.Fragment.HealthCareActivity;
@@ -16,13 +25,22 @@ import com.ronju.covid_19tracker.R;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+    NavigationView nav;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
     private long ID = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initView();
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
@@ -30,13 +48,15 @@ public class MainActivity extends AppCompatActivity {
             ID = getIntent().getExtras().getLong("ID");
         }
 
-
-
         transaction.replace(R.id.fragmentViewer, new HomeActivity(ID)).commit();
 
+
+
+
+
+
         bottomNavigationView = findViewById(R.id.bottom_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             public boolean onNavigationItemSelected(MenuItem item) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 switch (item.getItemId()) {
@@ -56,5 +76,30 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+    }
+
+
+
+    public void clickDrawerCloser(View view) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private void initView() {
+        toolbar = findViewById(R.id.toolbar);
+        nav = findViewById(R.id.nav_menu);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 }
