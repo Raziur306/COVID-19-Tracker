a46430d4a4a60bf03f7ccbe052012cbec939a135
Md. Raziur Rahaman Ronju <75040026+Raziur306@users.noreply.github.com>
2021-07-02 23:13:40 +0600

    Changed some item

--- app/src/main/java/com/ronju/covid_19tracker/Adapter/LatestUpdateAdapter.java
+++ app/src/main/java/com/ronju/covid_19tracker/Adapter/LatestUpdateAdapter.java
@@ -1,33 +1,66 @@
 package com.ronju.covid_19tracker.Adapter;
 
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
-
+import com.ronju.covid_19tracker.Model.WorldDataItem;
 import com.ronju.covid_19tracker.R;
+import java.util.ArrayList;
+import static android.view.View.GONE;
+import static com.ronju.covid_19tracker.DoInBackground.doInBackground.allCountryData;
 
 
 public class LatestUpdateAdapter extends RecyclerView.Adapter<LatestUpdateAdapter.ViewHolder> {
+    private View view;
+    private ArrayList<WorldDataItem> latestData = new ArrayList<>();
+    public LatestUpdateAdapter()
+    {
+        for(WorldDataItem item: allCountryData)
+        {
+            if(item.getTodayCases()> 0)
+            {
+                latestData.add(item);
+            }
+        }
+    }
+
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.latest_update_recycler_bg,parent,false);
+
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.latest_update_recycler_bg,parent,false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(LatestUpdateAdapter.ViewHolder holder, int position) {
-
+        if(latestData.get(position).getTodayCases()>0)
+        {
+            holder.latestNewsNewCase.setText(String.valueOf(latestData.get(position).getTodayCases()));
+            holder.latestNewsCountryName.setText(latestData.get(position).getCountryName());
+            if(latestData.get(position).getTodayDeaths()>0)
+            {
+                holder.latestNewsDeath.setText(String.valueOf(latestData.get(position).getTodayDeaths()));
+            }
+            else
+            {
+                holder.latestNewsDeath.setVisibility(GONE);
+                holder.temp_1.setVisibility(GONE);
+                holder.temp_2.setVisibility(GONE);
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return latestData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+
         TextView latestNewsNewCase, temp_1, temp_2, latestNewsDeath, latestNewsCountryName;
 
         public ViewHolder(View itemView) {
