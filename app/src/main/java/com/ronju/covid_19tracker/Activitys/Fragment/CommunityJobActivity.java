0ff8bf98b24ee5f991c791e54466e8eee22f9479
Md. Raziur Rahaman Ronju <75040026+Raziur306@users.noreply.github.com>
2021-07-12 21:46:00 +0600

    Added: New Fragment

---
+++ app/src/main/java/com/ronju/covid_19tracker/Activitys/Fragment/CommunityJobActivity.java
@@ -0,0 +1,14 @@
+package com.ronju.covid_19tracker.Activitys.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunityJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_job);
+    }
+}
